1b5ebc123d812a5f1bb85dfbf5c8b81a1b13537b
Zhiping Jiang <zpj@xidian.edu.cn>
2026-05-07 18:30:28 +0800

    Add CHS wheel packaging workflow

--- py-polars/src/polars/_utils/polars_version.py
+++ py-polars/src/polars/_utils/polars_version.py
@@ -1,7 +1,12 @@
 try:
     import polars._plr as plr
 
-    _POLARS_VERSION = f"{plr.__version__}+chs.autocache.0"
+    _CHS_VERSION_SUFFIX = "+chs.autocache.0"
+    _POLARS_VERSION = (
+        plr.__version__
+        if plr.__version__.endswith(_CHS_VERSION_SUFFIX)
+        else f"{plr.__version__}{_CHS_VERSION_SUFFIX}"
+    )
 except ImportError:
     # This is only useful for documentation
     import warnings
